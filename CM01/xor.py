26228bf2ce36deafb6e8c479e856d9d0a53afc7c
theamandawang <52474776+theamandawang@users.noreply.github.com>
2021-04-16 22:18:33 -0400

    Add files via upload

---
+++ CM01/xor.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+f = open('cm 01 frame.txt', 'r')
+f2 = open('cm01 code qr.txt', 'r')
+f3 = open('xored.txt', 'w+')
+array = np.zeros((301, 301))
+
+# each qr code is 301 by 301 using dcode.fr
+code = np.zeros((301,301))
+count = 0
+count2 = 0
+for line in f2:
+    count2 = 0
+    for x in line:
+        if(x == '0'):
+            code[count][count2] = 0
+        else:
+            code[count][count2]  = 1
+        count2 += 1
+    count += 1
+print(code) 
+
+frame = np.zeros((301,301))
+count = 0
+count2 = 0
+for line in f:
+    count2 = 0
+    for x in line:
+        if(x == '0'):
+            frame[count][count2] = 0
+        else:
+            frame[count][count2]  = 1
+        count2 += 1
+    count += 1
+print(frame) 
+
+for x in range(301):
+    for y in range(301):
+        if code[x][y] != frame[x][y]:
+            array[x][y] = 1
+
+for x in range(301):
+    f3.write(str(array[x]) + '\n')
